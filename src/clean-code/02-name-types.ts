a88caa1f3adf8e90a95f06338ac43a12013dc743
tbnicolas <tnicolas@uninorte.edu.co>
2023-11-04 11:05:33 -0500

    Nombre de variables

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,53 @@
+(() => {
+
+    const tempeturesInCelsius = [33.6, 12.34];
+
+    const serverIp = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const userEmails = users.map( user => user.email );
+
+    const canJump = false;
+    const canRun = true;
+    const hasItems = false;
+    const isLoading = false;
+
+    const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    const elapsedDate = new Date().getTime() - startTime;
+
+
+    // Funciones
+    // Obtiene los libros
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    // obtiene libros desde un URL
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    // obtiene el área de un cuadrado basado en sus lados
+    function getSquareArea( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    // imprime el trabajo
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
+
+
+
+
